0208f45003f3dabce93f55b068e55e078598a7eb
Kuehl <jan_markus.kuehl@web.de>
2023-12-22 10:46:35 +0100

    Comment: commented main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 public class Main {
     private static ArrayList<ArrayList<Integer>> board; // Das Spielbrett, repräsentiert als 2D-ArrayList
     private static int inputSizeBoard; // Größe des Spielbretts
-    private static int nullX, nullY; // Position des leeren Feldes (Reihe, Spalte)
+    private static int nullRow, nullCol; // Position des leeren Feldes (Reihe, Spalte)
     private static int moveCount; // Anzahl der Züge
 
     public static void main(String[] args) {
@@ -37,7 +37,7 @@ public class Main {
             System.out.println("Puzzle gelöst mit " + moveCount + " Zügen!");
             printBoard();
             System.out.print("Möchtest du noch eine Runde spielen? (ja/nein): ");
-            playAgain = scanner.next().trim().equalsIgnoreCase("ja");
+            playAgain = scanner.next().trim().equalsIgnoreCase("ja"); // wenn Spieler ja eingibt -> gibt true zurück
         }
         scanner.close();
         System.out.println("Spiel beendet. Danke fürs Spielen!");
@@ -53,8 +53,8 @@ public class Main {
 
         // 1-Dimensionaler Array erstellt, um die Zahlen darin mischen zu können
         ArrayList<Integer> numbersArray = new ArrayList<>();
-        for (int x = 1; x < inputSizeBoard * inputSizeBoard; x++) {
-            numbersArray.add(x); //  N x X Zahlen adden
+        for (int i = 1; i < inputSizeBoard * inputSizeBoard; i++) {
+            numbersArray.add(i); //  N x X Zahlen adden
         }
 
         numbersArray.add(null); // Null ist leeres Feld
@@ -63,18 +63,18 @@ public class Main {
         /* Initialisieren des 2D-Arrays */
         int indexNumbersArray = 0; // Aktuelle Position im numbersArray
 
-        for (int x = 0; x < inputSizeBoard; x++) {
-            ArrayList<Integer> row = new ArrayList<>();
+        for (int row = 0; row < inputSizeBoard; row++) {
+            ArrayList<Integer> rowArray = new ArrayList<>();
 
-            for (int y = 0; y < inputSizeBoard; y++) {
-                row.add(numbersArray.get(indexNumbersArray)); // Fügt ArrayList zahlen aus numbersArray hinzu
+            for (int col = 0; col < inputSizeBoard; col++) {
+                rowArray.add(numbersArray.get(indexNumbersArray)); // Fügt ArrayList zahlen aus numbersArray hinzu
                 if (numbersArray.get(indexNumbersArray) == null) {
-                    nullX = x; // erster Index des Leeren Feldes (Reihe)
-                    nullY = y; // zweiter Index des leeren Feldes (Spalte)
+                    nullRow = row; // erster Index des Leeren Feldes (Reihe)
+                    nullCol = col; // zweiter Index des leeren Feldes (Spalte)
                 }
                 indexNumbersArray++;
             }
-            board.add(row); // fügt dem board die jeweilige Reihe (i) hinzu
+            board.add(rowArray); // fügt dem board die jeweilige Reihe (i) hinzu
         }
         moveCount = 0; //setzt den Spielzug zurück
     }
@@ -94,26 +94,26 @@ public class Main {
     // Versucht, eine Zahl zu bewegen, und gibt zurück, ob der Zug gültig war
     private static boolean move(int numberToBoMoved) {
         // Durchlaufen der Zeilen um das leere Feld herum
-        int topFromNull = Math.max(0, nullX - 1);
-        int bottomFromNull = Math.min(inputSizeBoard - 1, nullX + 1);
-        int leftFromNull = Math.max(0, nullY - 1);
-        int rightFromNull = Math.min(inputSizeBoard - 1, nullY + 1);
+        int topFromNull = Math.max(0, nullRow - 1);
+        int bottomFromNull = Math.min(inputSizeBoard - 1, nullRow + 1);
+        int leftFromNull = Math.max(0, nullCol - 1);
+        int rightFromNull = Math.min(inputSizeBoard - 1, nullCol + 1);
 
 
-        for (int x = topFromNull; x <= bottomFromNull; x++) {
+        for (int row = topFromNull; row <= bottomFromNull; row++) {
             // Durchlaufen der Spalten um das leere Feld herum
-            for (int y = leftFromNull; y <= rightFromNull; y++) {
+            for (int col = leftFromNull; col <= rightFromNull; col++) {
                 // Überprüfung, ob die aktuelle Zelle die gesuchte Zahl enthält und nicht leer ist
-                if (board.get(x).get(y) != null && board.get(x).get(y) == numberToBoMoved) {
+                if (board.get(row).get(col) != null && board.get(row).get(col) == numberToBoMoved) {
                     // Überprüfung, ob die Zahl in derselben Zeile oder Spalte wie das leere Feld liegt
-                    if (x == nullX || y == nullY) {
+                    if (row == nullRow || col == nullCol) {
                         // Verschieben der Zahl in das leere Feld
-                        board.get(nullX).set(nullY, numberToBoMoved);
+                        board.get(nullRow).set(nullCol, numberToBoMoved);
                         // Aktualisieren der alten Position der Zahl auf 'null' (leeres Feld)
-                        board.get(x).set(y, null);
+                        board.get(row).set(col, null);
                         // Aktualisieren der Position des leeren Feldes
-                        nullX = x;
-                        nullY = y;
+                        nullRow = row;
+                        nullCol = col;
                         // Erhöhen des Spielzugszählers
                         moveCount++;
                         // Rückgabe von 'true', da der Zug erfolgreich war
@@ -130,10 +130,10 @@ public class Main {
     private static boolean isSolved() {
         //Überprüft die Anordnung 1-8 + 0
         int expectedNumber = 1; // Aufsteigende Reihenfolge, zahlen fangen im Spielbrett bei 1 an
-        for (int x = 0; x < inputSizeBoard; x++) {
-            for (int y = 0; y < inputSizeBoard; y++) {
+        for (int row = 0; row < inputSizeBoard; row++) {
+            for (int col = 0; col < inputSizeBoard; col++) {
                //prüft, ob es sich nicht um das letzte feld handelt ([2][2]) und ob es entweder null ist, oder nicht der erwarteten Zahl entspricht. Falls doch gibt es false zurück
-                if ((x != inputSizeBoard - 1 || y != inputSizeBoard - 1) && (board.get(x).get(y) == null || board.get(x).get(y) != expectedNumber)) {
+                if ((row != inputSizeBoard - 1 || col != inputSizeBoard - 1) && (board.get(row).get(col) == null || board.get(row).get(col) != expectedNumber)) {
                     return false;
                 }
                 expectedNumber++; // erhöht Zahl um 1
