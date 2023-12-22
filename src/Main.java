95adef70f37240082da7798721b93780919f4bee
cwichert <celinaawichert@gmail.com>
2023-12-22 10:02:17 +0100

    adjusted comments and renames

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 
 public class Main {
     private static ArrayList<ArrayList<Integer>> board; // Das Spielbrett, repräsentiert als 2D-ArrayList
-    private static int sizeBoard; // Größe des Spielbretts
+    private static int inputSizeBoard; // Größe des Spielbretts
     private static int emptyRow, emptyCol; // Position des leeren Feldes (Reihe, Spalte)
     private static int moveCount; // Anzahl der Züge
 
@@ -13,12 +13,12 @@ public class Main {
 
         // Spielbrettgröße einlesen
         System.out.print("Bitte gib die Größe des Spielbretts ein (z.B. 3 für ein 3x3-Spielbrett): ");
-        sizeBoard = scanner.nextInt();
+        inputSizeBoard = scanner.nextInt();
 
         // Prüft ob Eingabe mind. 3 ist
-        while (sizeBoard < 2) {
+        while (inputSizeBoard < 2) {
             System.out.println("Die Größe muss mindestens 2 sein. Bitte gib eine gültige Größe ein:");
-            sizeBoard = scanner.nextInt();
+            inputSizeBoard = scanner.nextInt();
         }
 
         boolean playAgain = true;
@@ -34,7 +34,7 @@ public class Main {
                 }
             }
 
-            System.out.println("Puzzle gelöst in " + moveCount + " Zügen!");
+            System.out.println("Puzzle gelöst mit " + moveCount + " Zügen!");
             printBoard();
             System.out.print("Möchtest du noch eine Runde spielen? (ja/nein): ");
             playAgain = scanner.next().trim().equalsIgnoreCase("ja");
@@ -43,26 +43,36 @@ public class Main {
         System.out.println("Spiel beendet. Danke fürs Spielen!");
     }
 
-    // Initialisiert das Spielbrett mit zufälligen Zahlen und einem leeren Feld
+    /**
+     * Initialisiert das Spielbrett zu Beginn eines neuen Spiels
+     * mit zufälligen Zahlen und einem leeren Feld.
+     */
     private static void initializeBoard() {
+        // 2-Dimensional Array erstellt
         board = new ArrayList<>();
-        ArrayList<Integer> numbersArray = new ArrayList<>();
-        for (int i = 1; i < sizeBoard * sizeBoard; i++) {
-            numbersArray.add(i);
-        }
-        numbersArray.add(null); // Null repräsentiert das leere Feld
-        Collections.shuffle(numbersArray); //mischt das array
 
-        int positionNumbersArray = 0; // Zählvariable, aktuelle
-        for (int i = 0; i < sizeBoard; i++) {
+        // 1-Dimensionaler Array erstellt, um die Zahlen darin mischen zu können
+        ArrayList<Integer> numbersArray = new ArrayList<>();
+        for (int x = 1; x < inputSizeBoard * inputSizeBoard; x++) {
+            numbersArray.add(x); //  N x X Zahlen adden
+        }
+
+        numbersArray.add(null); // Null ist leeres Feld
+        Collections.shuffle(numbersArray); // Mischt 1D-Array
+
+        /* Initialisieren des 2D-Arrays */
+        int indexNumbersArray = 0; // Aktuelle Position im numbersArray
+
+        for (int x = 0; x < inputSizeBoard; x++) {
             ArrayList<Integer> row = new ArrayList<>();
-            for (int j = 0; j < sizeBoard; j++) {
-                row.add(numbersArray.get(positionNumbersArray)); // Fügt dem reihen ArrayList zahlen aus numbersArray hinzu
-                if (numbersArray.get(positionNumbersArray) == null) {
-                    emptyRow = i; // erster Index des Leeren Feldes (Reihe)
-                    emptyCol = j; // zweiter Index des leeren Feldes (Spalte)
+
+            for (int y = 0; y < inputSizeBoard; y++) {
+                row.add(numbersArray.get(indexNumbersArray)); // Fügt ArrayList zahlen aus numbersArray hinzu
+                if (numbersArray.get(indexNumbersArray) == null) {
+                    emptyRow = x; // erster Index des Leeren Feldes (Reihe)
+                    emptyCol = y; // zweiter Index des leeren Feldes (Spalte)
                 }
-                positionNumbersArray++;
+                indexNumbersArray++;
             }
             board.add(row); // fügt dem board die jeweilige Reihe (i) hinzu
         }
@@ -71,8 +81,10 @@ public class Main {
 
     // Gibt das Spielbrett aus
     private static void printBoard() {
+        // Durchläuft jedes Element in jeder Zeile und gibt es in der Konsole aus
         for (ArrayList<Integer> row : board) {
             for (Integer num : row) {
+                // bedingung ? ausdruck_wenn_wahr : ausdruck_wenn_falsch
                 System.out.print(num == null ? "  " : num + " ");
             }
             System.out.println();
@@ -80,15 +92,18 @@ public class Main {
     }
 
     // Versucht, eine Zahl zu bewegen, und gibt zurück, ob der Zug gültig war
-    private static boolean move(int number) {
-        for (int i = Math.max(0, emptyRow - 1); i <= Math.min(sizeBoard - 1, emptyRow + 1); i++) {
-            for (int j = Math.max(0, emptyCol - 1); j <= Math.min(sizeBoard - 1, emptyCol + 1); j++) {
-                if (board.get(i).get(j) != null && board.get(i).get(j) == number) {
-                    if (i == emptyRow || j == emptyCol) {
-                        board.get(emptyRow).set(emptyCol, number);
-                        board.get(i).set(j, null);
-                        emptyRow = i;
-                        emptyCol = j;
+    private static boolean move(int numberToBeMoved) {
+        int topFromNull = Math.max(0, emptyRow - 1);
+        int bottomFromNull = Math.min(inputSizeBoard - 1, emptyRow + 1);
+        for (int row = topFromNull; row <= bottomFromNull; row++) {
+
+            for (int column = Math.max(0, emptyCol - 1); column <= Math.min(inputSizeBoard - 1, emptyCol + 1); column++) {
+                if (board.get(row).get(column) != null && board.get(row).get(column) == numberToBeMoved) {
+                    if (row == emptyRow || column == emptyCol) {
+                        board.get(emptyRow).set(emptyCol, numberToBeMoved);
+                        board.get(row).set(column, null);
+                        emptyRow = row;
+                        emptyCol = column;
                         moveCount++;
                         return true;
                     }
@@ -100,10 +115,11 @@ public class Main {
 
     // Überprüft, ob das Puzzle gelöst ist
     private static boolean isSolved() {
-        int expected = 1;
-        for (int i = 0; i < sizeBoard; i++) {
-            for (int j = 0; j < sizeBoard; j++) {
-                if ((i != sizeBoard - 1 || j != sizeBoard - 1) && (board.get(i).get(j) == null || board.get(i).get(j) != expected)) {
+        //Überprüft die Anordnung 1-8 + 0
+        int expected = 1; // Aufsteigende Reihenfolge
+        for (int x = 0; x < inputSizeBoard; x++) {
+            for (int y = 0; y < inputSizeBoard; y++) {
+                if ((x != inputSizeBoard - 1 || y != inputSizeBoard - 1) && (board.get(x).get(y) == null || board.get(x).get(y) != expected)) {
                     return false;
                 }
                 expected++;
